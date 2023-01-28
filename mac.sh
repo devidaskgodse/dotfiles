bcfe0fa5a11413ba711c28b462a3119ffb4ab79d
Devidas Godse <devidaskgodse@gmail.com>
2023-01-29 01:37:51 +0530

    added mac installer script

---
+++ mac.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Macbook with Apple Silicon
+# Installation guide: https://mac.install.guide/ruby/3.html
+# install homebrew package manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# add homebrew to path
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/devidas/.zprofile                    
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/devidas/.zprofile               
+eval "$(/opt/homebrew/bin/brew shellenv)"                                            
+
+# Setting up git
+git config --global user.name "Devidas Godse"
+git config --global user.email "devidaskgodse@gmail.com"
+
+# Installing applications
+brew install gh nvim lammps tmux gnuplot ffmpeg gimp 
+
+# Fonts
+brew tap homebrew/cask-fonts
+brew install --cask font-hack
+brew install --cask font-comic-mono
+
+# install manually
+# brave-browser anydesk sublime-text sublime-merge msteams obsidian ovito r julia raindrop miniconda
+
+# python packages
+conda install numpy scipy sympy numba matplotlib seaborn scikit-learn scikit-image pandas jupyter jupyterlab
+
+# install from appstore
+# whatsapp davinci-resolve keynote numbers pages ticktick
+
+
+# rust packages
+
+
+# julia packages
